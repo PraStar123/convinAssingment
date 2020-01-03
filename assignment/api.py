b2f08202ee1f80902ad6ae24a0e7a1c9bed0794e
PraStar123 <praneet1998@gmail.com>
2020-01-03 23:43:15 +0530

    Final commit

--- assignment/api.py
+++ assignment/api.py
@@ -1,5 +1,6 @@
 from rest_framework import routers
 from myapp import views
 
+# For routing REST requests
 router = routers.DefaultRouter()
 router.register(r'filesAPI', views.FileViewSet)
--- myapp/views.py
+++ myapp/views.py
@@ -2,15 +2,22 @@ from django.shortcuts import render, redirect
 from .models import FileModel
 from .forms import FileForm, FileUpdateForm
 from django.contrib import messages
+from django.http import Http404, HttpResponse
+from django.conf import settings
 from .serializers import FileSerializer, OperationSerializer
 from rest_framework.response import Response
 from rest_framework.parsers import FileUploadParser
 from rest_framework.views import APIView
 from rest_framework import generics, status 
 from rest_framework import viewsets
+import os 
+
 
 
 # Create your views here.
+# Home Page views
+def home(request):
+    return redirect('files')
 
 # For viewing all the files in database
 def files(request):
@@ -64,6 +71,20 @@ def deleteFile(request, pk):
     if file:
         messages.info(request, f"'{file.fileName}' has been deleted")
         file.delete()
+    return redirect('files')
+
+# For downloading a file
+def downloadFile(request, pk):
+    file = FileModel.objects.get(pk=pk)
+    if file: 
+        givenPath = os.path.join(settings.MEDIA_ROOT, file.filePath.path)
+        if os.path.exists(givenPath):
+            with open(givenPath, 'rb') as fh:
+                response = HttpResponse(fh.read(), content_type="application/text")
+                response['Content-Disposition'] = 'inline; filename=' + os.path.basename(givenPath)
+                return response
+    else: 
+        raise Http404
     return redirect('files')
 
 # For uploading file via REST api
